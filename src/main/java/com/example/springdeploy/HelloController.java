c9bc7e8a73e3579e831f1e11fb83cabab825082e
Nicolas Santos <nico99esp@gmail.com>
2023-06-21 18:29:49 +0200

    Spring heroku app config

---
+++ src/main/java/com/example/springdeploy/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.springdeploy;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Value("${app.message}")
+    String message;
+
+    @GetMapping("/hola")
+    public String holaMundo(){
+        System.out.println(message);
+        return "Hola Giussepe, quiere pizza?";
+    }
+
+}
